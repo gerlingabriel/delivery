40336399af436a222423229c9a4887f7cd740dd5
Gabriel Gerlin <gerlin.gabriel@gmail.com>
2021-08-22 13:56:09 -0300

    Retirar pasta nosql

--- nosql/src/main/java/com/project/nosql/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.nosql.repository;
-
-import com.project.nosql.model.User;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String>{
-    
-}
